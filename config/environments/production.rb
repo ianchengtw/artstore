1bf23aafa8ae0ffb637affd417527d0ce1f3df6f
ianchengtw <chang.baoh@gmail.com>
2016-03-27 01:35:25 +0800

    heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'https://arcane-tor-23111.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'artstore-iancheng.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
