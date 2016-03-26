04da8781c5d1a47c55342118129a3485936e92dd
ianchengtw <chang.baoh@gmail.com>
2016-03-27 02:05:31 +0800

    cash flow "pay2go ATM"

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :pay2go_cc_notify
-  protect_from_forgery except: :pay2go_cc_notify
+  before_action :authenticate_user!, except: [:pay2go_cc_notify, :pay2go_atm_complete]
+  protect_from_forgery except: [:pay2go_cc_notify, :pay2go_atm_complete]
 
   def show
     @order = Order.find_by_token(params[:id])
@@ -46,10 +46,28 @@ class OrdersController < ApplicationController
       @order.make_payment!
 
       if @order.is_paid?
-        flash[:notice] = 'Pay with credit card successfully'
-        redirect_to account_orders_path
+        redirect_to order_path(@order.token), notice: 'Pay successfully'
       else
         render text: 'Pay with credit card failed'
+      end
+    else
+      render text: 'Trade failed'
+    end
+  end
+
+  def pay2go_atm_complete
+    @order = Order.find_by_token(params[:id])
+    json_data = JSON.parse(params['JSONData'])
+
+    if json_data['Status'] == 'SUCCESS'
+      @order.reserve_goods!
+      @order.set_payment_with!('atm')
+      @order.make_payment!
+
+      if @order.is_paid?
+        redirect_to order_path(@order.token), notice: 'Pay successfully'
+      else
+        render text: 'Pay with ATM failed'
       end
     else
       render text: 'Trade failed'
